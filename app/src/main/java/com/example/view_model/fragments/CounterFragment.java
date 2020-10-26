020d1a9626491d6aa9e7bf063d973d3c00bfaff4
revindaamalias <revindaamalia@gmail.com>
2020-10-26 17:50:42 +0700

    view model dan angka tetap

--- app/src/main/java/com/example/view_model/fragments/CounterFragment.java
+++ app/src/main/java/com/example/view_model/fragments/CounterFragment.java
@@ -30,80 +30,13 @@ public class CounterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        FragmentCounterBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_counter, container, false);
+        FragmentCounterBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_counter, container, false);
         mViewModels = new ViewModelProvider(this).get(CounterViewModels.class);
 
         // line diatas merupakan cara untuk menghubungkan counterFragment dengan counter view models.
 //        kita set up
         binding.setCounterViewModel(mViewModels);
-        //home
-
-//        sekarang kita mengubah binding.score sekarang menjaddi mViewmodels
-        binding.btnThreePointHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setHomeScore(mViewModels.score.getHomeScore()+3);
-                Log.d("Home Score :", String.valueOf(mViewModels.score.getHomeScore()));
-                binding.txtScoreHome.setText(Integer.toString(mViewModels.score.getHomeScore()));
-            }
-        });
-
-        binding.btnTwoPointHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setHomeScore(mViewModels.score.getHomeScore()+2);
-                Log.d("Home Score :", String.valueOf(mViewModels.score.getHomeScore()));
-                binding.txtScoreHome.setText(Integer.toString(mViewModels.score.getHomeScore()));
-            }
-        });
-
-        binding.btnFreeThrowHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setHomeScore(mViewModels.score.getHomeScore()+1);
-                Log.d("Home Score :", String.valueOf(mViewModels.score.getHomeScore()));
-                binding.txtScoreHome.setText(Integer.toString(mViewModels.score.getHomeScore()));
-            }
-        });
-
-        //away
-        binding.btnFreeThrowAway.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setAwayScore(mViewModels.score.getAwayScore()+1);
-                Log.d("Away Score :", String.valueOf(mViewModels.score.getAwayScore()));
-                binding.txtScoreAway.setText(Integer.toString(mViewModels.score.getAwayScore()));
-            }
-        });
-
-        binding.btnTwoPointAway.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setAwayScore(mViewModels.score.getAwayScore()+2);
-                Log.d("Away Score :", String.valueOf(mViewModels.score.getAwayScore()));
-                binding.txtScoreAway.setText(Integer.toString(mViewModels.score.getAwayScore()));
-            }
-        });
-
-        binding.btnThreePointAway.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setAwayScore(mViewModels.score.getAwayScore()+3);
-                Log.d("Away Score :", String.valueOf(mViewModels.score.getAwayScore()));
-                binding.txtScoreAway.setText(Integer.toString(mViewModels.score.getAwayScore()));
-            }
-        });
-
-        binding.btnResetScore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewModels.score.setAwayScore(0);
-                mViewModels.score.setHomeScore(0);
-                binding.txtScoreAway.setText(Integer.toString(mViewModels.score.getAwayScore()));
-                binding.txtScoreHome.setText(Integer.toString(mViewModels.score.getHomeScore()));
-            }
-        });
-
+        binding.setLifecycleOwner(this);
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/view_model/viewmodels/CounterViewModels.java
+++ app/src/main/java/com/example/view_model/viewmodels/CounterViewModels.java
@@ -1,5 +1,7 @@
 package com.example.view_model.viewmodels;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 import com.example.view_model.models.Score;
@@ -8,6 +10,20 @@ public class CounterViewModels extends ViewModel {
 //    data yang biasanya ada di fragment pindah kesini.
     public Score score = new Score(0,0);
 //    karena kita menggunakan public jadi kita bisa
+//    step 3: jika homeScore : 0 dan awayScore: 0 di observe, dia bisa update otomatis
+//    dan tidak menetap pada angka 0.
+
+    public void addPointHome(int point){
+        score.setHomeScore(score.getHomeScore()+point);
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+    }
+
+    public void addPointAway(int point){
+        score.setAwayScore(score.getAwayScore()+point);
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+    }
+//    step 3: jika di run, nilainya tetap 0 tetapi, jiak kita lihat dilogcat, ada nilainya.
+//    karena data bindingnya tidak bisa meng observe pada fragment counter.xml
 }
 
 //Note
